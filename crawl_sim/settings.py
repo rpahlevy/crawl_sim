936585c1d730460cb55aa1fcb9a131872ee3796d
rpahlevy <43176811+rpahlevy@users.noreply.github.com>
2020-04-25 06:52:52 +0700

    update setting untuk single crawl

--- crawl_sim/settings.py
+++ crawl_sim/settings.py
@@ -22,15 +22,15 @@ NEWSPIDER_MODULE = 'crawl_sim.spiders'
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 1000
+CONCURRENT_REQUESTS = 100
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 5
+# DOWNLOAD_DELAY = 5
 DOWNLOAD_TIMEOUT = 30
 # The download delay setting will honor only one of:
-CONCURRENT_REQUESTS_PER_DOMAIN = 5
+# CONCURRENT_REQUESTS_PER_DOMAIN = 5
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
