54e1fd1587e9911032c84ed786c7d33845802b03
rpahlevy <43176811+rpahlevy@users.noreply.github.com>
2020-06-05 12:57:31 +0700

    fix out of memory dengan konsep priority queue dibatasi total

--- datasets_preprocess.py
+++ datasets_preprocess.py
@@ -67,10 +67,12 @@ with jsonlines.open(file_datasets) as f:
         if data['retweet_count'] <= 5:
             continue
         datasets.append(data)
+        datasets = sorted(datasets, key=itemgetter('retweet_count'), reverse=True)
+        if len(datasets) > total:
+            datasets.pop()
         if index % 1000 == 999:
             print('read {} data'.format(index + 1))
 
-    datasets = sorted(datasets, key=itemgetter('retweet_count'), reverse=True)
     for index, data in enumerate(datasets):
         if index < start_row:
             continue
